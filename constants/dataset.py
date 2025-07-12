5dc230bad56e99c674596f8cf9ad6197ba88f89f
Eric Xu <exu.data@gmail.com>
2025-07-11 20:09:24 -0400

    Reorganize borrower lender relationship page navigation.

--- constants/dataset.py
+++ constants/dataset.py
@@ -1,7 +1,7 @@
-LOCATION = "MSA: Trenton-Princeton, NJ"
+LOCATION = "Los Angeles, CA"
 
-START_DATE = "2025-01-20"
-END_DATE = "2025-02-03"
+START_DATE = "2024-07-06"
+END_DATE = "2025-07-06"
 
 PARTY_TO_COUNTERPARTY = {"borrower": "lender", "lender": "borrower"}
 PARTY_TO_DATASET_KEY = {"borrower": "buyerName", "lender": "lenderName"}
