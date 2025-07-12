10faaca7129f2249edf157715e197fbf402b23da
Eric Xu <exu.data@gmail.com>
2025-07-12 09:37:41 -0400

    Add secrets to secrets.toml

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
-import os
-
+import streamlit as st
 import streamlit_analytics2 as streamlit_analytics
-from dotenv import load_dotenv
 from streamlit.navigation.page import StreamlitPage
 
 from utils.st_app import initialize_session_state, load_css, setup_page_navigation
-
-load_dotenv()
 
 
 class MultiApp:
@@ -21,7 +17,7 @@ class MultiApp:
 
 
 with streamlit_analytics.track(
-    unsafe_password=os.getenv("STREAMLIT_ANALYTICS_PASSWORD", "test123")
+    unsafe_password=st.secrets.get("st_analytics_password", "test123")
 ):
     app = MultiApp()
     app.run()
