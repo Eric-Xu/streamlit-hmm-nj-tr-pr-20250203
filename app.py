e093e8aba3dd9822c93052befe54afdd9bc1353b
Eric Xu <exu.data@gmail.com>
2025-07-12 01:10:22 -0400

    Switch to use streamlit-analytics2

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+import os
+
+import streamlit_analytics2 as streamlit_analytics
+from dotenv import load_dotenv
 from streamlit.navigation.page import StreamlitPage
 
 from utils.st_app import initialize_session_state, load_css, setup_page_navigation
+
+load_dotenv()
 
 
 class MultiApp:
@@ -14,5 +20,8 @@ class MultiApp:
         pg.run()
 
 
-app = MultiApp()
-app.run()
+with streamlit_analytics.track(
+    unsafe_password=os.getenv("STREAMLIT_ANALYTICS_PASSWORD", "test123")
+):
+    app = MultiApp()
+    app.run()
